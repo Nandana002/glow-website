82bd77e5b98cb56599887e6b7a64ceb1c61e20d1
Nandana K <nandanakodiveettil@gmail.com>
2026-01-13 16:08:50 +0530

    another changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv"
 import {app}from "./app.js"
 import connectDB from "./db/index.js"
-dotenv.config({
-    path:"./.env"
-})
+
+dotenv.config();
+
 
 
 const PORT =process.env.PORT||7000
